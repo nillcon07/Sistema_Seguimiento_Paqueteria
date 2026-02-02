9f61cc1398b046de81573e0388f08fb60c4d0d2d
Valentin <valennillcon@gmail.com>
2026-02-01 21:22:17 -0300

    Update main_postgres.py

--- main_postgres.py
+++ main_postgres.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, HTMLResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse 
 from pydantic import BaseModel
 from typing import Optional
 import logica_envios_postgres as logic
